d21c0c57c5c950c1b636e4c6be481e41ca5c938d
Suresh Khatri <shresh.khatri@gmail.com>
2021-08-15 13:58:02 +0100

    some trivial changes

--- python_files/helper.py
+++ python_files/helper.py
@@ -4,7 +4,10 @@ def timestamp_to_datetime(timestamp):
     return datetime.fromtimestamp(timestamp).strftime('%Y %m %d %H: %M')
     
 
-def format_datetime(conversation_list):
-    for aConversation in conversation_list:
-        if 'latest_event_time' in aConversation:
-            aConversation['latest_event_time']=timestamp_to_datetime(aConversation['latest_event_time'])
+def format_datetime(itemList):
+    for anItem in itemList:
+        if 'latest_event_time' in anItem:
+            anItem['latest_event_time']=timestamp_to_datetime(anItem['latest_event_time'])
+        elif 'events' in anItem:
+            anItem.events.timestamp=timestamp_to_datetime(anItem.events.timestamp)
+
