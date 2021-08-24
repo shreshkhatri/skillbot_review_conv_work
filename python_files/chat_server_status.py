b606c4f6fd6a12eab270af3299f69ff38736ce27
Suresh Khatri <shresh.khatri@gmail.com>
2021-08-24 22:34:07 +0100

    one positional argument specified as domain name for chat server

--- python_files/chat_server_status.py
+++ python_files/chat_server_status.py
@@ -1,10 +1,11 @@
 import sys
 import requests,json
 
+
 #get list of entities
-def check_server_status():
+def check_server_status(server_domain):
     try:
-        response=requests.get('http://localhost:5005/',headers={'Content-Type':'text/plain','Accept': '*/*'})
+        response=requests.get(server_domain,headers={'Content-Type':'text/plain','Accept': '*/*'})
         return True if response.status_code==200 else False
-    except Exception as exc:
-        return 'Error : Chat server seems offline please turn it on'
+    except Exception as instance:
+        return 'Error : '+str(instance)
